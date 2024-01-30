efc9960f8ca6de982ab913f06fc8a137f7a0cc3d
Jordi Cabañes Santa <140833687+Jordikick@users.noreply.github.com>
2024-01-30 20:04:44 +0100

    Add files via upload

---
+++ health_checks.py
@@ -0,0 +1,15 @@
+psutil()
+print(psutil)
+import shutil
+import psutil
+def check_disk_usage(disk):
+    du=shutil.disk_usage(disk)
+    free=du.free/du.total*100
+    return free>20
+def check_cpu_usage():
+    usage=psutil.cpu_percent(1)
+    return usage<70
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("ERROR!")
+else:
+    print("Everything is OK!")
